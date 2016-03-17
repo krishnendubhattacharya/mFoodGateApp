0b1fc292f34752336fd243934ca341be7eccdfbf
nits.krishnendu <nits.krishnendu@gmail.com>
2016-03-17 18:41:58 +0530

    17/03/2016  anup chakraborty

--- app/Controllers/homeCtrl.js
+++ app/Controllers/homeCtrl.js
@@ -16,6 +16,73 @@ app.controller('homeCtrl', function ($rootScope, $scope, $http, $location) {
         });
     }
     $scope.catList();
+
+    $scope.featuredPromoList = function(){
+
+        $http({
+            method: "GET",
+            url: $rootScope.serviceurl + "getFeaturedCategories",
+        }).success(function (data) {
+            $scope.featuredCatInfo =data.featuredcat;
+            //console.log($scope.featuredCatInfo);
+
+        });
+    }
+    $scope.featuredPromoList();
+
+    $scope.newPromoList = function(){
+
+        $http({
+            method: "GET",
+            url: $rootScope.serviceurl + "getLaunchTodayPromo",
+        }).success(function (data) {
+            $scope.newPromoInfo =data.todayPromo;
+            $scope.newPromoCount =data.count;
+            //console.log($scope.newPromoInfo);
+
+        });
+    }
+    $scope.newPromoList();
+
+    $scope.hotSellingList = function(){
+
+        $http({
+            method: "GET",
+            url: $rootScope.serviceurl + "getFeaturedCategories",
+        }).success(function (data) {
+            $scope.hotSellingInfo =data.featuredcat;
+            //console.log($scope.newPromoInfo);
+
+        });
+    }
+    $scope.hotSellingList();
+
+    $scope.lastDayPromoList = function(){
+
+        $http({
+            method: "GET",
+            url: $rootScope.serviceurl + "getLastdayPromo",
+        }).success(function (data) {
+            $scope.lastDayPromoInfo =data.lastdayPromo;
+            //console.log($scope.newPromoInfo);
+
+        });
+    }
+    $scope.lastDayPromoList();
+
+
+    $scope.restaurantList = function(){
+
+        $http({
+            method: "GET",
+            url: $rootScope.serviceurl + "getLastdayPromo",
+        }).success(function (data) {
+            $scope.restaurantInfo =data.lastdayPromo;
+            //console.log($scope.newPromoInfo);
+
+        });
+    }
+    $scope.restaurantList();
    
 });
 
