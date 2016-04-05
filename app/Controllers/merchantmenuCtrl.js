77f40f456acb876f058a4f2f70c0be7e0f6164ed
nitskrishnendu0811 <nits.krishnendu@gmail.com>
2016-04-05 20:31:42 +0530

    Updates 05/04/2016 By Bikash

--- app/Controllers/merchantmenuCtrl.js
+++ app/Controllers/merchantmenuCtrl.js
@@ -1,4 +1,4 @@
-app.controller('merchantmenuCtrl', function ($rootScope, $scope, $http, $location, $stateParams, myAuth) {
+app.controller('merchantmenuCtrl', function ($rootScope, $scope, $http, $location, $stateParams, myAuth, $timeout) {
     myAuth.updateUserinfo(myAuth.getUserAuthorisation());
     $scope.loggedindetails = myAuth.getUserNavlinks();
     $scope.voucherInfo;
@@ -6,112 +6,263 @@ app.controller('merchantmenuCtrl', function ($rootScope, $scope, $http, $locatio
 
         $location.path("/login");
     }
-
-
+    $scope.img_uploader = null;
+    $scope.textBox = {image:  {
+            buttonText: 'Select file',
+            labelText: 'Drop file here',
+            multiple: false,
+            accept: 'image/*',
+            uploadUrl: $rootScope.serviceurl + 'menuFileUpload',
+            onUploaded:function(ret){
+                $scope.menuInfo.image = ret.file.value.name;
+                console.log(ret.file.value,ret.file.value.name);
+            },
+            onInitialized : function(e)
+            {
+                $scope.img_uploader = e.component;
+            }
+    },
+        price:{
+            mode: "number"
+        }};
 
 
     $scope.changeView = function(){
         $scope.edit_mode = !$scope.edit_mode;
         $scope.menuInfo = {
+            id:'',
             title:'',
             description:'',
             price:'',
             status:false
         }
+        //$scope.textBox.image.value = null;
+        //$scope.img_uploader = ;
+        $scope.img_uploader.reset();
+
     }
 
-    $scope.getPoints = function() {
-        $scope.edit_mode = false;
-        $http({
-            method: "GET",
-            url: $rootScope.serviceurl + "mypoints/" + $scope.loggedindetails.id,
-        }).success(function (data) {
-            $scope.voucherInfo = data.data;
-            //console.log($scope.voucherInfo);
-            $scope.dataGridOptions = {
-                dataSource: $scope.voucherInfo,
-                selection: {
-                    mode: "single"
-                },
-                paging: {
-                    pageSize: 5
-                },
-                pager: {
-                    showPageSizeSelector: true,
-                    allowedPageSizes: [5, 10, 20],
-                    showInfo: true
-                },
-
-                columns: ["sl", "points","redeemed_points", "remaining_points" ,"source", "date","expire_date"
-                    /*{
-                     width: 100,
-                     alignment: 'center',
-                     cellTemplate: function (container, options) {
-
-                     $('<button/>').addClass('dx-button')
-                     .text('Details')
-                     .on('dxclick', function () {
-                     $location.path('/voucherdetail/' + options.data.id);
-                     })
-                     .appendTo(container);
-                     }
-                     },
-                     {
-                     width: 100,
-                     alignment: 'center',
-                     cellTemplate: function (container, options) {
-                     $('<button/>').addClass('dx-button')
-                     .text('ReSell')
-                     .on('dxclick', function () {
-                     //Do something with options.data;
-                     $location.path('/vouchersell/' + options.data.id);
-                     })
-                     .appendTo(container);
-
-                     }
-                     }*/
-
-                ]
-            };
-        });
+    $scope.edit_menu = function(menu)
+    {
+        console.log(menu);
+        $scope.edit_mode = !$scope.edit_mode;
+        $scope.menuInfo = {
+            id:menu.id,
+            title:menu.title,
+            description:menu.description,
+            price:menu.price,
+            status:menu.status=="Active"?true:false,
+            imageurl:menu.imageurl
+        }
+        $scope.img_uploader.reset();
     }
-    $scope.getPoints();
+    $scope.voucherInfo = null;
+    $scope.datag = null;
 
-    $scope.save_menu = function(){
-        console.log($scope.menuInfo);
-        $scope.menuInfo.user_id = $scope.loggedindetails.id;
-        $http({
-            method: "POST",
-            url: $rootScope.serviceurl+"addMenu",
-            data: $scope.menuInfo,
-            headers: {'Content-Type': 'application/json'},
-        }).success(function(data) {
-            console.log(data);
-            if(data.type == 'success'){
-                var message = data.message;
-                //params.validationGroup.reset();
-                //$location.path('/login');
-                //$scope.getUserInfo();
-                //$location.path('/merchantoffer');
+    $scope.dataGridOptions = {
+        dataSource: $scope.voucherInfo,
+
+        selection: {
+            mode: "single"
+        },
+        paging: {
+            pageSize: 5
+        },
+        pager: {
+            showPageSizeSelector: true,
+            allowedPageSizes: [5, 10, 20],
+            showInfo: true
+        },
+        onInitialized : function(e){
+            console.log('By Bikash  --  ',e);
+            $scope.datag = e.component;
+        },
+        columns: ["sl", "title","price", {
+            caption:'Image',
+            cellTemplate: function (container, options) {
+                if(options.data.image) {
+                    $('<img />')
+                        .height(100)
+                        .attr('src', options.data.imageurl)
+                        .appendTo(container);
+                }
+            }
+        },"status",
+            {
+                caption:'Edit',
+                width: 100,
+                alignment: 'center',
+                cellTemplate: function (container, options) {
+                    $('<button/>').addClass('dx-button')
+                        .text('Edit')
+                        .on('dxclick',function(){$scope.edit_menu(options.data); })
+                        .appendTo(container);
+                }
+            },
+            {
+                caption:'Delete',
+                width: 100,
+                alignment: 'center',
+                cellTemplate: function (container, options) {
+                    $('<button/>').addClass('dx-button')
+                        .text('Delete')
+                        .on('dxclick',function(){$scope.delete_menu(options.data); })
+                        .appendTo(container);
+                }
+            }/*,
+             {
+             width: 100,
+             alignment: 'center',
+             cellTemplate: function (container, options) {
+             $('<button/>').addClass('dx-button')
+             .text('ReSell')
+             .on('dxclick', function () {
+             //Do something with options.data;
+             $location.path('/vouchersell/' + options.data.id);
+             ghghgh(options.data.id);
+             })
+             .appendTo(container);
+
+             }
+             }*/
+
+        ]
+    };
+
+    $scope.delete_menu = function(data)
+    {
+        if(confirm('Are you sure you want to delete this menu?'))
+        {
+            $http({
+                method: "delete",
+                url: $rootScope.serviceurl + "deleteMenu/" + data.id,
+            }).success(function (data) {
                 DevExpress.ui.notify({
-                    message: "Added Successfilly",
+                    message: "Deleted Successfilly",
                     position: {
                         my: "center top",
                         at: "center top"
                     }
                 }, "success", 3000);
-            }else{
-                var message = "Error occured.";
-                DevExpress.ui.notify({
-                    message: message,
-                    position: {
-                        my: "center top",
-                        at: "center top"
-                    }
-                }, "error", 3000);
-            }
+                //$scope.edit_mode = !$scope.edit_mode;
+                $scope.getMenus();
+            })
+        }
+    }
 
-        })
+    $scope.getMenus = function() {
+        //$scope.dataGridOptions = null;
+        $scope.edit_mode = false;
+        $http({
+            method: "GET",
+            url: $rootScope.serviceurl + "getMenuByUser/" + $scope.loggedindetails.id,
+        }).success(function (data) {
+            $scope.voucherInfo = data.data;
+            //console.log($scope.voucherInfo);
+            //if($scope.datag)
+                //$scope.datag.option({dataSource:$scope.voucherInfo});
+
+
+            //$scope.refresh_grid();
+            //$timeout(function(){
+                //$scope.datag.refresh();
+            //},3000)
+
+
+        });
+    }
+    $scope.getMenus();
+
+
+
+   $scope.$watchCollection('voucherInfo', function() {
+        //var dataGrid = angular.element('#gridContainer').dxDataGrid('instance');
+        //console.log($scope.voucherInfo);
+        //if(dataGrid)
+            //dataGrid.option({dataSource:$scope.voucherInfo});
+       //console.log('1st');
+       if($scope.datag) {
+           $scope.datag.option({dataSource:$scope.voucherInfo});
+           //console.log('2nd');
+       }
+    });
+
+    $scope.save_menu = function(){
+
+        //console.log($scope.textBox.image.value,$scope.menuInfo);
+        if($scope.menuInfo.id)
+        {
+            $http({
+                method: "POST",
+                url: $rootScope.serviceurl+"updateMenu",
+                data: $scope.menuInfo,
+                headers: {'Content-Type': 'application/json'},
+            }).success(function(data) {
+                //console.log(data);
+                if(data.type == 'success'){
+                    var message = data.message;
+                    //params.validationGroup.reset();
+                    //$location.path('/login');
+                    //$scope.getUserInfo();
+                    //$location.path('/merchantoffer');
+                    DevExpress.ui.notify({
+                        message: "Updated Successfilly",
+                        position: {
+                            my: "center top",
+                            at: "center top"
+                        }
+                    }, "success", 3000);
+                    $scope.edit_mode = !$scope.edit_mode;
+                    $scope.getMenus();
+                }else{
+                    var message = "Error occured.";
+                    DevExpress.ui.notify({
+                        message: message,
+                        position: {
+                            my: "center top",
+                            at: "center top"
+                        }
+                    }, "error", 3000);
+                }
+            })
+        }
+        else
+        {
+            $scope.menuInfo.user_id = $scope.loggedindetails.id;
+            $http({
+                method: "POST",
+                url: $rootScope.serviceurl+"addMenu",
+                data: $scope.menuInfo,
+                headers: {'Content-Type': 'application/json'},
+            }).success(function(data) {
+                //console.log(data);
+                if(data.type == 'success'){
+                    var message = data.message;
+                    //params.validationGroup.reset();
+                    //$location.path('/login');
+                    //$scope.getUserInfo();
+                    //$location.path('/merchantoffer');
+                    DevExpress.ui.notify({
+                        message: "Added Successfilly",
+                        position: {
+                            my: "center top",
+                            at: "center top"
+                        }
+                    }, "success", 3000);
+                    $scope.edit_mode = !$scope.edit_mode;
+                    $scope.getMenus();
+                }else{
+                    var message = "Error occured.";
+                    DevExpress.ui.notify({
+                        message: message,
+                        position: {
+                            my: "center top",
+                            at: "center top"
+                        }
+                    }, "error", 3000);
+                }
+            })
+        }
     }
 
 
