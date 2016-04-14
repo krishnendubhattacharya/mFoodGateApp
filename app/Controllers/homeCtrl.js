52ff25d14ff431353382588894da2c1432fada19
nits.krishnendu <nits.krishnendu@gmail.com>
2016-04-14 12:30:11 +0530

    14/04/2016  anup chakraborty

--- app/Controllers/homeCtrl.js
+++ app/Controllers/homeCtrl.js
@@ -4,137 +4,142 @@
  */
 app.controller('homeCtrl', function ($rootScope, $scope, $http, $location) {
 
-    $scope.catList = function(){
+    $scope.showAll = false;
+    $scope.showAllFunc = function() {
+        $scope.catList = function () {
 
-        $http({
-            method: "GET",
-            url: $rootScope.serviceurl + "getCategories",
-        }).success(function (data) {
-            $scope.catInfo =data.category;
-            console.log($scope.catInfo);
+            $http({
+                method: "GET",
+                url: $rootScope.serviceurl + "getCategories",
+            }).success(function (data) {
+                $scope.catInfo = data.category;
+                console.log($scope.catInfo);
 
-        });
+            });
+        }
+        $scope.catList();
+
+        $scope.featuredPromoList = function () {
+
+            $http({
+                method: "GET",
+                url: $rootScope.serviceurl + "getFeaturedCategories",
+            }).success(function (data) {
+                $scope.featuredCatInfo = data.featuredcat;
+                //console.log($scope.featuredCatInfo);
+
+            });
+        }
+        $scope.featuredPromoList();
+
+        $scope.newPromoList = function () {
+
+            $http({
+                method: "GET",
+                url: $rootScope.serviceurl + "getLaunchTodayPromo",
+            }).success(function (data) {
+                $scope.newPromoInfo = data.todayPromo;
+                $scope.newPromoCount = data.count;
+                //console.log($scope.newPromoInfo);
+
+            });
+        }
+        $scope.newPromoList();
+
+        $scope.hotSellingList = function () {
+
+            $http({
+                method: "GET",
+                url: $rootScope.serviceurl + "getHotSellingPromo",
+            }).success(function (data) {
+                $scope.hotSellingInfo = data.getHotSellingPromo;
+                //console.log($scope.newPromoInfo);
+
+            });
+        }
+        $scope.hotSellingList();
+
+        $scope.lastDayPromoList = function () {
+
+            $http({
+                method: "GET",
+                url: $rootScope.serviceurl + "getLastdayPromo",
+            }).success(function (data) {
+                $scope.lastDayPromoInfo = data.lastdayPromo;
+                //console.log($scope.newPromoInfo);
+
+            });
+        }
+        $scope.lastDayPromoList();
+
+
+        $scope.restaurantList = function () {
+
+            $http({
+                method: "GET",
+                url: $rootScope.serviceurl + "getFeaturedResturantHome",
+            }).success(function (data) {
+                $scope.restaurantInfo = data.restaurants;
+                //console.log($scope.restaurantInfo);
+
+            });
+        }
+        $scope.restaurantList();
+
+        $scope.specialList = function () {
+
+            $http({
+                method: "GET",
+                url: $rootScope.serviceurl + "getSpecialPromo",
+            }).success(function (data) {
+                $scope.specialInfo = data.getSpecialPromo;
+                //console.log($scope.specialInfo);
+
+            });
+        }
+        $scope.specialList();
+
+        $scope.menuPromoList = function () {
+
+            $http({
+                method: "GET",
+                url: $rootScope.serviceurl + "getMenuPromo",
+            }).success(function (data) {
+                $scope.menuPromoInfo = data.getMenuPromo;
+                //console.log($scope.menuPromoInfo);
+
+            });
+        }
+        $scope.menuPromoList();
+
+        $scope.paymentPromoList = function () {
+
+            $http({
+                method: "GET",
+                url: $rootScope.serviceurl + "getPaymentPromo",
+            }).success(function (data) {
+                $scope.paymentPromoInfo = data.getPaymentPromo;
+                //console.log($scope.menuPromoInfo);
+
+            });
+        }
+        $scope.paymentPromoList();
+
+        $scope.merchamtMembershipPromoList = function () {
+
+            $http({
+                method: "GET",
+                url: $rootScope.serviceurl + "getMerchantPromo",
+            }).success(function (data) {
+                $scope.merchantMembershipPromoInfo = data.getMerchantPromo;
+                //console.log($scope.menuPromoInfo);
+
+            });
+        }
+        $scope.merchamtMembershipPromoList();
+        $scope.showAll = true;
     }
-    $scope.catList();
-
-    $scope.featuredPromoList = function(){
-
-        $http({
-            method: "GET",
-            url: $rootScope.serviceurl + "getFeaturedCategories",
-        }).success(function (data) {
-            $scope.featuredCatInfo =data.featuredcat;
-            //console.log($scope.featuredCatInfo);
-
-        });
-    }
-    $scope.featuredPromoList();
-
-    $scope.newPromoList = function(){
-
-        $http({
-            method: "GET",
-            url: $rootScope.serviceurl + "getLaunchTodayPromo",
-        }).success(function (data) {
-            $scope.newPromoInfo =data.todayPromo;
-            $scope.newPromoCount =data.count;
-            //console.log($scope.newPromoInfo);
-
-        });
-    }
-    $scope.newPromoList();
-
-    $scope.hotSellingList = function(){
-
-        $http({
-            method: "GET",
-            url: $rootScope.serviceurl + "getHotSellingPromo",
-        }).success(function (data) {
-            $scope.hotSellingInfo =data.getHotSellingPromo;
-            //console.log($scope.newPromoInfo);
-
-        });
-    }
-    $scope.hotSellingList();
-
-    $scope.lastDayPromoList = function(){
-
-        $http({
-            method: "GET",
-            url: $rootScope.serviceurl + "getLastdayPromo",
-        }).success(function (data) {
-            $scope.lastDayPromoInfo =data.lastdayPromo;
-            //console.log($scope.newPromoInfo);
-
-        });
-    }
-    $scope.lastDayPromoList();
-
-
-    $scope.restaurantList = function(){
-
-        $http({
-            method: "GET",
-            url: $rootScope.serviceurl + "getFeaturedResturantHome",
-        }).success(function (data) {
-            $scope.restaurantInfo =data.restaurants;
-            //console.log($scope.restaurantInfo);
-
-        });
-    }
-    $scope.restaurantList();
-
-    $scope.specialList = function(){
-
-        $http({
-            method: "GET",
-            url: $rootScope.serviceurl + "getSpecialPromo",
-        }).success(function (data) {
-            $scope.specialInfo =data.getSpecialPromo;
-            //console.log($scope.specialInfo);
-
-        });
-    }
-    $scope.specialList();
-
-    $scope.menuPromoList = function(){
-
-        $http({
-            method: "GET",
-            url: $rootScope.serviceurl + "getMenuPromo",
-        }).success(function (data) {
-            $scope.menuPromoInfo =data.getMenuPromo;
-            //console.log($scope.menuPromoInfo);
-
-        });
-    }
-    $scope.menuPromoList();
-
-    $scope.paymentPromoList = function(){
-
-        $http({
-            method: "GET",
-            url: $rootScope.serviceurl + "getPaymentPromo",
-        }).success(function (data) {
-            $scope.paymentPromoInfo =data.getPaymentPromo;
-            //console.log($scope.menuPromoInfo);
-
-        });
-    }
-    $scope.paymentPromoList();
-
-    $scope.merchamtMembershipPromoList = function(){
-
-        $http({
-            method: "GET",
-            url: $rootScope.serviceurl + "getMerchantPromo",
-        }).success(function (data) {
-            $scope.merchantMembershipPromoInfo =data.getMerchantPromo;
-            //console.log($scope.menuPromoInfo);
-
-        });
-    }
-    $scope.merchamtMembershipPromoList();
+    $scope.showAllFunc();
    
 });
 
--- app/Controllers/membershipCtrl.js
+++ app/Controllers/membershipCtrl.js
@@ -18,6 +18,62 @@ app.controller('membershipCtrl', function ($rootScope, $scope, $http, $location)
     }
     $scope.membershipList();
 
+    $scope.newPromoList = function () {
+
+        $http({
+            method: "GET",
+            url: $rootScope.serviceurl + "getLaunchTodayMembership",
+        }).success(function (data) {
+            $scope.newPromoInfo = data.todayPromo;
+            $scope.newPromoCount = data.count;
+            //console.log($scope.newPromoInfo);
+
+        });
+    }
+    $scope.newPromoList();
+
+    $scope.hotSellingList = function () {
+
+        $http({
+            method: "GET",
+            url: $rootScope.serviceurl + "getHotSellingMembership",
+        }).success(function (data) {
+            $scope.hotSellingInfo = data.getHotSellingPromo;
+            //console.log($scope.newPromoInfo);
+
+        });
+    }
+    $scope.hotSellingList();
+
+    $scope.lastDayPromoList = function () {
+
+        $http({
+            method: "GET",
+            url: $rootScope.serviceurl + "getLastdayMembership",
+        }).success(function (data) {
+            $scope.lastDayPromoInfo = data.lastdayPromo;
+            //console.log($scope.newPromoInfo);
+
+        });
+    }
+    $scope.lastDayPromoList();
+
+
+
+
+    $scope.specialList = function () {
+
+        $http({
+            method: "GET",
+            url: $rootScope.serviceurl + "getSpecialMembership",
+        }).success(function (data) {
+            $scope.specialInfo = data.getSpecialPromo;
+            //console.log($scope.specialInfo);
+
+        });
+    }
+    $scope.specialList();
+
 
 
 
