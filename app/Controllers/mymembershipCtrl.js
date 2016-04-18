c614b487518828fa3e5f50801a777f0e37276722
nitskrishnendu0811 <nits.krishnendu@gmail.com>
2016-04-18 20:44:15 +0530

    Updates 18/04/2016 Day End By Bikash

--- app/Controllers/mymembershipCtrl.js
+++ app/Controllers/mymembershipCtrl.js
@@ -56,9 +56,66 @@ app.controller('mymembershipCtrl', function ($rootScope, $scope, $http, $locatio
 
         });
 
+    $scope.getExpiredMemberships = function(){
+        $http({
+            method: "GET",
+            url: $rootScope.serviceurl + "getMyExpiredMembership/"+$scope.loggedindetails.id,
+        }).success(function (data) {
+            $scope.voucherInfo =data;
+            //console.log($scope.voucherInfo.membership);
+            $scope.listViewData1.option({"dataSource": $scope.voucherInfo.membership,showSelectionControls: true });
+            $scope.dataGridOptions1 = {
+                dataSource: $scope.voucherInfo.membership,
+                selection: {
+                    mode: "single"
+                },
+                paging: {
+                    pageSize: 5
+                },
+                pager: {
+                    showPageSizeSelector: true,
+                    allowedPageSizes: [5, 10, 20],
+                    showInfo: true
+                },
+
+                columns: [{ dataField: 'title',caption:'Description'},
+                    { dataField: 'restaurant.title',caption:'Restaurant'},
+                    { dataField: 'purchased_date',caption:'Member Since'}, "expire_date"
+                    /*{
+                        width: 100,
+                        alignment: 'center',
+                        cellTemplate: function (container, options) {
+
+                            $('<button/>').addClass('dx-button')
+                                .text('Details')
+                                .on('dxclick', function () {
+                                    $location.path('/membershipdetail/'+options.data.voucher_id);
+                                })
+                                .appendTo(container);
+                        }
+                    }*/
+
+                ],
+                onSelectionChanged: function (selectedItems) {
+                    //console.log(selectedItems);
+                    //$scope.selectedEmployee = selectedItems.selectedRowsData[0];
+                }
+            };
+
+
+        });
+    }
+
+    $scope.getExpiredMemberships();
+
     $scope.loadList=function(e)
     {
         $scope.listViewData= e.component;
+    }
+
+    $scope.loadList1=function(e)
+    {
+        $scope.listViewData1= e.component;
     }
 
     $scope.view_det = function (id) {
