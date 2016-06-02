b8568b052853e46d4498bc7e6c88f3ae4b1396f5
nitskrishnendu0811 <nits.krishnendu@gmail.com>
2016-06-02 21:08:23 +0530

    Updates 2/06/2016 By Bikash

--- app/Controllers/websitemenusCtrl.js
+++ app/Controllers/websitemenusCtrl.js
@@ -1,4 +1,4 @@
-app.controller('websitemenusCtrl', function ($rootScope, $scope, $http, $location, $stateParams, myAuth) {
+app.controller('websitemenusCtrl', function ($rootScope, $scope, $http, $location, $stateParams, myAuth,$timeout) {
     myAuth.updateUserinfo(myAuth.getUserAuthorisation());
     $scope.loggedindetails = myAuth.getUserNavlinks();
     $scope.voucherInfo;
@@ -27,6 +27,44 @@ app.controller('websitemenusCtrl', function ($rootScope, $scope, $http, $locatio
             headers: {'Content-Type': 'application/json'},
         }).success(function(data) {
             $scope.sliderImages = data.data;
+            $timeout(function(){
+                // $('#ca-container').contentcarousel();
+                var carousal = $('.newcar');
+                carousal.owlCarousel({
+                    autoplay:true,
+                    touchDrag:false,
+                    loop:($scope.sliderImages && $scope.sliderImages.length>1?true:false),
+                    dots:true,
+                    nav:true,
+                    navContainerClass:"ca-nav",
+                    navText:false,
+                    autoplayTimeout:5000,
+                    autoplayHoverPause:true,
+                    singleItem:true,
+                    responsive:{
+                        0:{
+                            items:1
+                        },
+                        600:{
+                            items:2
+                        },
+                        1000:{
+                            items:2
+                        }
+                    }
+                });
+                //$('.item,.ca-item-main,.desc,.desc.txt,.desc.view-btn').on('mouseout', function (e){
+                //    console.log('hii');
+                //    carousal.trigger('play.owl.autoplay',[5000]);
+                //});
+                //$('.item,.ca-item-main,.desc,.desc.txt,.desc.view-btn').on('mouseover', function (e){
+                //    carousal.trigger('stop.owl.autoplay');
+                //});
+                //
+                //$('.item,.ca-item-main,.desc,.desc.txt,.desc.view-btn').on('mouseleave', function (e){
+                //    carousal.trigger('play.owl.autoplay');
+                //});
+            },30);
         })
     }
     $scope.getSliderImages();
