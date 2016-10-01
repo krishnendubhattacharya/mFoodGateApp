56dba6caceb71151d2c47ed40d72afba543b68c5
nitskrishnendu0811 <nits.krishnendu@gmail.com>
2016-10-01 20:06:09 +0530

    update by bikash 30-9-16

--- app/Controllers/footerCtrl.js
+++ app/Controllers/footerCtrl.js
@@ -16,5 +16,33 @@ app.controller('footerCtrl', function ($rootScope, $scope, $http, $location) {
        })
    }
     $scope.getSocialLinks();
+    $scope.sendemailtoadmin = function(){
+        console.log($scope.subscribeemail);
+        $http({
+            method: "GET",
+            url: $rootScope.serviceurl+"sendSubscribeEmail/"+$scope.subscribeemail,
+            //data:{subscribe_email:$scope.subscribeemail},
+            headers: {'Content-Type': 'application/json'},
+        }).success(function(data) {
+            $scope.subscribeemail = '';
+            if(data.type='success'){
+                DevExpress.ui.notify({
+                    message: data.message,
+                    position: {
+                        my: "center top",
+                        at: "center top"
+                    }
+                }, "success", 3000);
+            }else{
+                DevExpress.ui.notify({
+                    message: data.message,
+                    position: {
+                        my: "center top",
+                        at: "center top"
+                    }
+                }, "error", 3000);
+            }
+        })
+    }
 });
 
